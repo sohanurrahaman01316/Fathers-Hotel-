651e33f554a942ee7502304eabbdfe92c706f044
Sohanur Rahaman <sohanurrahaman01316@gmail.com>
2022-11-24 15:28:53 +0600

    first commit

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,5 @@
 // DRY ---> Do Not Repeat Yourself
-document.getElementById("btn-deposit").addEventListener("click", function () {
+document.getElementById("btn-deposit").addEventListener("click",function(){ 
   /* 
     1. get the element by id
     2. get the value from the element
@@ -14,12 +14,13 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
 
   // calculate new deposit total
   const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
   // set deposit total value
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
+  const previousBalanceTotal = getTextElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal + newDepositAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
+}
 );
